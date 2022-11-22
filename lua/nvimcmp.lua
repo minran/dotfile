5a3eba7ad29608267bb708bd19b468df8e1cba19
wh.huajieyu <wang.shuang_202000@immomo.com>
2022-11-22 17:32:13 +0800

    resolve rust generic problem

--- lua/nvimcmp.lua
+++ lua/nvimcmp.lua
@@ -63,9 +63,9 @@ cmp.setup({
           omni = "[Omni]",
         },
       })(entry, vim_item)
-      local strings = vim.split(kind.kind, "%s", { trimempty = true })
-      kind.kind = " " .. strings[1] .. " "
-      kind.menu = "    (" .. strings[2] .. ")"
+      --local strings = vim.split(kind.kind, "%s", { trimempty = true })
+      --kind.kind = " " .. strings[1] .. " "
+      --kind.menu = "    (" .. strings[2] .. ")"
       return kind
     end,
   },
