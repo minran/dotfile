4ffe473869a8c747052c1a207ad982307f298dff
wh.huajieyu <wang.shuang_202000@immomo.com>
2023-05-10 10:49:11 +0800

    delete sumneko-lua

--- lua/config/nvimmason.lua
+++ lua/config/nvimmason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-   ensure_installed = { "yamlls", "sumneko_lua", "rust_analyzer", "volar", "tsserver", "solidity" }
+   ensure_installed = { "yamlls", "rust_analyzer", "volar", "tsserver", "solidity" }
 })
