b37f7038ceb3c288667be63197f51601c8fc3675
wh.huajieyu <wang.shuang_202000@immomo.com>
2023-06-21 14:45:21 +0800

    update

--- lua/config/nvim_leap.lua
+++ lua/config/nvim_leap.lua
@@ -2,9 +2,9 @@ require('leap').add_default_mappings()
 
 require('leap').opts.highlight_unlabeled_phase_one_targets = true
 
-vim.keymap.set({'x', 'o', 'n'}, 'r', '<Plug>(leap-forward-to)')
+vim.keymap.set({'x', 'o', 'n'}, 's', '<Plug>(leap-forward-to)')
 
-vim.keymap.set({'x', 'o', 'n'}, 'R', '<Plug>(leap-backward-to)')
+vim.keymap.set({'x', 'o', 'n'}, 'S', '<Plug>(leap-backward-to)')
 
 require('flit').setup {
   keys = { f = 'f', F = 'F', t = 't', T = 'T' },
