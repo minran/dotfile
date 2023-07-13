81d3d182f31c48ef739d4848301e8ae2b1fe64db
wh.huajieyu <wang.shuang_202000@immomo.com>
2023-07-13 17:51:38 +0800

    delete lsp format plugin

--- lua/config/nvim_mason.lua
+++ lua/config/nvim_mason.lua
@@ -1,5 +1,5 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-   ensure_installed = { "clangd", "yamlls", "cssls", "html", "lua_ls", "rust_analyzer", "volar", "tsserver", "solidity" },
-   automatic_installation = true,
+  ensure_installed = { "clangd", "yamlls", "cssls", "html", "lua_ls", "rust_analyzer", "volar", "tsserver", "solidity" },
+  automatic_installation = true,
 })
