2a75c4b1a1da7cc05f6a4eede87809b77fc5c2c7
minran <1113629699@qq.com>
2023-07-25 22:30:12 +0800

    change theme to dark

--- init.lua
+++ init.lua
@@ -4,11 +4,11 @@ require("plugin_config")
 --require("plugins")
 
 vim.o.termguicolors = true
-vim.cmd [[colorscheme vscode]]
+-- vim.cmd [[colorscheme vscode]]
 --vim.cmd[[colorscheme onelight]]
 --vim.cmd[[colorscheme tokyonight]]
 --vim.cmd[[colorscheme monokai]]
---vim.cmd[[colorscheme sonokai]]
+vim.cmd[[colorscheme sonokai]]
 
 vim.cmd('syntax on')
 vim.cmd('filetype plugin indent on')
@@ -41,8 +41,8 @@ set.hlsearch = true   -- 搜索时高亮显示被找到的文本
 --set.showmatch = true
 set.cursorline = true -- 高亮当前行
 
---set.background="dark" -- 黑色
-set.background = "light" -- 白色
+set.background="dark" -- 黑色
+-- set.background = "light" -- 白色
 
 -- 缩进2个空格等于一个Tab
 vim.o.tabstop = 2
