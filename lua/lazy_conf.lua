447635d5aad3f0baac56ba2f444766dd3e312405
wh.huajieyu <wang.shuang_202000@hellogroup.com>
2024-05-11 10:55:53 +0800

    Update

--- lua/lazy_conf.lua
+++ lua/lazy_conf.lua
@@ -195,4 +195,7 @@ require("lazy").setup({
 
   { "JoosepAlviste/nvim-ts-context-commentstring", commit = "6c30f3c8915d7b31c3decdfe6c7672432da1809d" },
 
+  ------------------ hlargs.nvim -----------------
+  { 'm-demare/hlargs.nvim' },
+
 })
